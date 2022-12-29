3f31f9060ef90dcdb2dab8c039bf06342cb193de
Sofia Bustos <dv.sofiabustos@gmail.com>
2022-12-29 09:34:41 -0300

    entrega lista

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ while (isNaN (anioNacimiento) || (!anioNacimiento)) {
 
 let edad = anioActual - anioNacimiento;
 
-if (edad >= 18) { alert("Bienvenid@ " + usuario + " tienes " + edad + " años," + " estas habilitado para navegar por esta pagina")} else { alert ( "No estás habilitado para navegar por esta página ya que no eres mayor de 18 años")
-
-}
+if (edad >= 18) { alert("Bienvenid@ " + usuario + " tienes " + edad + " años," + " estas habilitado para navegar por esta pagina")
 
 console.log(usuario);
 console.log(edad);
@@ -43,6 +41,7 @@ function sumarTotal (prod1=0 , prod2=0, prod3=0) {
     
 }
 
+
 let producto1 = Number (prompt ("¿Elija el precio del producto  que desea comprar? \n 1. 100 para Vinos \n 2.150 para Aperitivos  \n 3. 250 para Fernet  ") )
 let producto2 = Number (prompt ("¿Elija el precio del producto  que desea comprar? \n 1. 100 para Vinos \n 2.150 para Aperitivos  \n 3. 250 para Fernet  ") )
 let producto3 = Number (prompt ("¿Elija el precio del producto  que desea comprar? \n 1. 100 para Vinos \n 2.150 para Aperitivos  \n 3. 250 para Fernet  ") ) ;
@@ -52,7 +51,9 @@ let productosComprado = sumarTotal (producto1 + producto2 + producto3);
 
 alert ( "Gastaste " + "$" + productosComprado);
 
-let formaDePAgo = Number (prompt ("Ingrese la forma de pago \n 1 para pago en efectivo 20% de descuento \n 2 para pago con tarjeta de credito hasta 3 pagos con el 10% de recargo"))
+
+
+const formaDePAgo = Number (prompt ("Ingrese la forma de pago \n 1 para pago en efectivo 20% de descuento \n 2 para pago con tarjeta de credito hasta 3 pagos con el 10% de recargo"))
 
 const calcularDescuento = productosComprado => productosComprado * 0.20
 
@@ -65,6 +66,8 @@ const calcularRecargo = productosComprado => productosComprado * 0.10
 let recargoConTarjeta = calcularRecargo (productosComprado)
 
 let totalConTarjeta = productosComprado + calcularRecargo (productosComprado)
+
+
 if (formaDePAgo == 1 ) {
        
         alert ("el descuento es de $ " + descuentoEfectivo )
@@ -81,33 +84,16 @@ if (formaDePAgo == 2) {
 
 }
 
+} else { alert ( "No estás habilitado para navegar por esta página ya que no eres mayor de 18 años") 
+
+}
+alert ("Muchas gracias por visitar nuestra tienda")
 
 
 
 
 
-// formas de pago
 
-
-// switch (formaDePAgo) {
-//         case "1":
-//             return totalEnefectivo
-           
-//         case "2":
-//             return totalConTarjeta
-             
-           
-//         default: 
-//            return alert ("No se puede calcular el costo, intente nuevamente") 
-//     }
-    
-
-
-
-
-// let precioFinalConDescuento = calcularDescuento
-
-// let precioFinalMasInteres = calcularRecargo
 
 
 
